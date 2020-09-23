3b41c1cd765b4eb56001d89de051bc2a840bca46
chosanglyul <chosanglyul@gmail.com>
2020-09-23 23:21:13 +0900

    Add classes

--- subjects/classes.js
+++ subjects/classes.js
@@ -3,35 +3,31 @@ const isNumber = require("../lib/isNumber");
 
 module.exports = {
     post: async(ctx, next) => {
-        if(!ctx.request.body.name) ctx.throw(400);
-        if(!isNumber(ctx.request.body.hours, "4") || !isNumber(ctx.request.body.credit, "4")) ctx.throw(400);
-        if(ctx.request.body.hours <= 0 || ctx.request.body.credit <= 0) ctx.throw(400);
-        const isExist = await ctx.state.collection.subjects.countDocuments({ code: parseInt(ctx.params.code, 10)});
+        if(!ctx.request.body.teacher) ctx.throw(400);
+        const isExist = await ctx.state.collection.classes.countDocuments({ subjectcode: parseInt(ctx.params.subjectcode, 10), classnum: parseInt(ctx.params.classnum, 10) });
         if(isExist >= 1) ctx.throw(400);
-        await ctx.state.collection.subjects.findOneAndUpdate({ code: parseInt(ctx.params.code, 10) }, {
+        await ctx.state.collection.classes.findOneAndUpdate({ subjectcode: parseInt(ctx.params.subjectcode, 10), classnum: parseInt(ctx.params.classnum, 10) }, {
             $setOnInsert: {
-                name: ctx.request.body.name,
-                hours: parseInt(ctx.request.body.hours, 10),
-                credit: parseInt(ctx.request.body.credit, 10),
-                classcount: 0
+                teacher: ctx.request.body.teacher,
+                students: []
             }
         }, { upsert: true });
         await next();
     },
     get: async(ctx, next) => {
-        const subject = await ctx.state.collection.subjects.findOne({ code: parseInt(ctx.params.code, 10) });
-        if(!subject) ctx.throw(400);
-        ctx.body.data = subject;
+        const tmpclass = await ctx.state.collection.classes.findOne({ subjectcode: parseInt(ctx.params.subjectcode, 10), classnum: parseInt(ctx.params.classnum, 10) });
+        if(!tmpclass) ctx.throw(400);
+        ctx.body.data = tmpclass;
         await next();
     },
     delete: async(ctx, next) => {
-        const isExist = await ctx.state.collection.subjects.countDocuments({ code: parseInt(ctx.params.code, 10)});
+        const isExist = await ctx.state.collection.classes.countDocuments({ subjectcode: parseInt(ctx.params.subjectcode, 10), classnum: parseInt(ctx.params.classnum, 10) });
         if(!isExist) ctx.throw(400);
-        await ctx.state.collection.subjects.deleteOne({ code: parseInt(ctx.params.code, 10) });
+        await ctx.state.collection.classes.deleteOne({ subjectcode: parseInt(ctx.params.subjectcode, 10), classnum: parseInt(ctx.params.classnum, 10) });
         await next();
     },
     common: async(ctx, next) => {
-        if(!isNumber(ctx.params.code, "4") || ctx.params.code < 0) ctx.throw(400);
+        if(!isNumber(ctx.params.subjectcode, "4") || ctx.params.subjectcode < 0 || !isNumber(ctx.params.classnum, "4") || ctx.params.classnum < 0) ctx.throw(400);
         await next();
     }
 };
